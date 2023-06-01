2ca656b84bddffee2536d78e582daa6ce14289d4
Bailey Capuano <bcapuano@asu.edu>
2023-05-31 22:16:14 -0400

    Defect

--- mayhem/fuzz_nat.py
+++ mayhem/fuzz_nat.py
@@ -7,19 +7,13 @@ import fuzz_helpers
 with atheris.instrument_imports():
     import markdown_to_json
 
-ctr = 0
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
-    global ctr
-    ctr += 1
-    try:
-        ast = markdown_to_json.CommonMark.DocParser().parse(fdp.ConsumeRemainingString())
-        dictionary = markdown_to_json.CMarkASTNester().nest(ast)
-        dict(markdown_to_json.Renderer().stringify_dict(dictionary))
-    except TypeError:
-        if ctr > 1000:
-            raise
-        return -1
+
+    ast = markdown_to_json.CommonMark.DocParser().parse(fdp.ConsumeRemainingString())
+    dictionary = markdown_to_json.CMarkASTNester().nest(ast)
+    dict(markdown_to_json.Renderer().stringify_dict(dictionary))
+
 def main():
     atheris.Setup(sys.argv, TestOneInput)
     atheris.Fuzz()
